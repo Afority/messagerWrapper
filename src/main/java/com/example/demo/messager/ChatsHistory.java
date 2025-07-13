7ec9267a1578d5974d3e565e76280b34771fc36d
Almas <pukceji@gmail.com>
2025-07-13 14:59:22 +0500

    делаю историю чатов, не будет хранить всю, только какие апдейты пришли во время сессии

--- src/main/java/com/example/demo/messager/ChatsHistory.java
+++ src/main/java/com/example/demo/messager/ChatsHistory.java
@@ -1,4 +1,34 @@
 package com.example.demo.messager;
 
-public class ChatHistory {
+import com.example.demo.messager.dataClasses.ChatHistory;
+import com.example.demo.messager.dataClasses.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatsHistory {
+    static private final List<ChatHistory> chatHistoryList = new ArrayList<>();
+
+    static void addChat(ChatHistory chatHistory){
+        chatHistoryList.add(chatHistory);
+    }
+
+    static void updateMessage(int chatId, Message message){
+        try {
+            ChatHistory chatHistory = chatHistoryList.stream()
+                    .filter((chat)-> chat.getChatId() == chatId)
+                    .findFirst()
+                    .orElse(null);
+
+            if (chatHistory == null) {
+                ChatHistory newChat = new ChatHistory(chatId);
+                newChat.updateMessage(message);
+                chatHistoryList.add(newChat);
+                addChat(newChat);
+            }
+        }
+        catch (IndexOutOfBoundsException e){
+            System.err.println(e.getMessage());
+        }
+    }
 }
